a4d213f360d957ee24ba2835cc580ba6aca2473e
safiullah63 <safiullahkarimi.dev@gmail.com>
2025-10-13 09:23:29 +0430

    transaction part done

--- app/Filament/Import/Resources/BuyResource.php
+++ app/Filament/Import/Resources/BuyResource.php
@@ -320,6 +320,7 @@ class BuyResource extends Resource
         return $table
             ->columns([
             Tables\Columns\TextColumn::make('barcode')->label('بارکد')->numeric()->sortable(),
+            Tables\Columns\TextColumn::make('company.name')->label('نام شرکت')->numeric()->sortable()->searchable(),
             Tables\Columns\TextColumn::make('name')->label('نام جنس')->searchable(),
             Tables\Columns\TextColumn::make('unit')->label('نوع بسته بندی')->searchable(),
             Tables\Columns\TextColumn::make('price')->label('قیمت فی دانه')->numeric()->sortable(),
