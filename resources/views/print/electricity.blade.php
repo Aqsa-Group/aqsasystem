ace5a9c5e70e07601372ae73ec3b0806cfa13724
safiullah63 <safiullahkarimi.dev@gmail.com>
2025-11-24 10:14:25 +0430

    market panel updated

--- resources/views/print/electricity.blade.php
+++ resources/views/print/electricity.blade.php
@@ -271,46 +271,40 @@
             }
         }
 
-        @media print {
-            @page {
-                size: A5 portrait;
-                /* چاپ عمودی */
-                margin: 0;
-            }
+       @media print {
+    @page {
+        size: A4 portrait; /* چاپ عمودی */
+        margin: 0;
+    }
 
-            html,
-            body {
-                width: 210mm;
-                height: 297mm;
-                margin: 0;
-                padding: 0;
-            }
+    html, body {
+        width: 210mm;
+        height: 297mm;
+        margin: 0;
+        padding: 0;
+    }
 
-            .page-wrapper {
-                width: 100%;
-                height: 297mm;
-                /* تمام ارتفاع صفحه */
-                display: flex;
-                flex-direction: column;
-                margin: 0;
-                padding: 0;
-            }
+    .page-wrapper {
+        width: 100%;
+        height: 297mm; /* تمام ارتفاع صفحه */
+        display: flex;
+        flex-direction: column;
+        margin: 0;
+        padding: 0;
+    }
 
-            .receipt {
-                flex: 1;
-                /* هر قبض دقیقا یک‌سوم صفحه */
-                width: 100%;
-                padding: 10px;
-                box-sizing: border-box;
-                border-bottom: 2px dashed #999;
-                /* خط جداکننده برای بریدن */
-            }
+    .receipt {
+        flex: 1; /* هر قبض دقیقا یک‌سوم صفحه */
+        width: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        border-bottom: 2px dashed #999; /* خط جداکننده برای بریدن */
+    }
 
-            .receipt:last-child {
-                border-bottom: none;
-                /* برای قبض آخر */
-            }
-        }
+    .receipt:last-child {
+        border-bottom: none; /* برای قبض آخر */
+    }
+}
 
 
         /* دکمه‌های غیر چاپی */
@@ -341,6 +335,10 @@
 
 <body>
 
+    <div class="no-print">
+        <button class="btn" onclick="window.print()">🖨 چاپ</button>
+        <button class="btn close" onclick="window.close()">✕ بستن</button>
+    </div>
 
     <div class="page" role="main" aria-label="فرم پرداخت برق">
         <table class="two-col" role="table" aria-label="دو نسخه رسید">
@@ -379,13 +377,7 @@
                             <tr>
                                 <th style="font-weight:600;">مشتری</th>
                                 <th style="font-weight:600;">مارکت</th>
-                                <th style="font-weight:600;">
-                                    @if (!empty($accounting->shop->number) )
-                                    شماره دوکان
-                                    @else
-                                    شماره غرفه
-                                    @endif
-                                </th>
+                                <th style="font-weight:600;">شماره دوکان</th>
                                 <th style="font-weight:600;">شماره مسلسل</th>
                                 <th style="font-weight:600;">از تاریخ</th>
                                 <th style="font-weight:600;">تا تاریخ</th>
@@ -401,17 +393,14 @@
 
                                 <td>
                                     @if($accounting->paid_date)
-                                    {{ \Morilog\Jalali\Jalalian::fromDateTime($accounting->paid_date)->format('Y/m/d')
-                                    }}
+                                    {{ \Morilog\Jalali\Jalalian::fromDateTime($accounting->paid_date)->format('Y/m/d') }}
                                     @else
                                     ---
                                     @endif
                                 </td>
                                 <td>
                                     @if($accounting->expiration_date)
-                                    {{
-                                    \Morilog\Jalali\Jalalian::fromDateTime($accounting->expiration_date)->format('Y/m/d')
-                                    }}
+                                    {{ \Morilog\Jalali\Jalalian::fromDateTime($accounting->expiration_date)->format('Y/m/d') }}
                                     @else
                                     ---
                                     @endif
@@ -560,13 +549,7 @@
                             <tr>
                                 <th>مشتری</th>
                                 <th>مارکت</th>
-                                 <th style="font-weight:600;">
-                                    @if (!empty($accounting->shop->number) )
-                                    شماره دوکان
-                                    @else
-                                    شماره غرفه
-                                    @endif
-                                </th>
+                                <th>شماره دوکان</th>
                                 <th>شماره مسلسل</th>
 
 
@@ -646,9 +629,9 @@
         </table>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
-    <script>
-        function downloadPDF() {
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+<script>
+    function downloadPDF() {
         const element = document.querySelector(".page");
         const opt = {
             margin:       0.2,
@@ -672,7 +655,7 @@
     window.afterprint = function() {
         setTimeout(() => { window.close(); }, 1000);
     }
-    </script>
+</script>
 
 </body>
 
