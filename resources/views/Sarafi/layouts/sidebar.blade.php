3edd2ee468be254044fe661619efa183ac7982a0
safiullah63 <safiullahkarimi.dev@gmail.com>
2026-01-03 21:22:22 +0430

    panel edited

--- resources/views/Sarafi/layouts/sidebar.blade.php
+++ resources/views/Sarafi/layouts/sidebar.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" name="csrf-token"
         content="{{ csrf_token() }}">
     <title>سیستم صرافی اقصی</title>
-
+<link rel="icon" type="image/jpeg" href="{{ asset('assets/aqsa.jpg') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 
     <script>
--- storage/framework/views/ca0f831e847453632a5cf1f3d48b4900.php
+++ storage/framework/views/ca0f831e847453632a5cf1f3d48b4900.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" name="csrf-token"
         content="<?php echo e(csrf_token()); ?>">
     <title>سیستم صرافی اقصی</title>
-
+<link rel="icon" type="image/jpeg" href="<?php echo e(asset('assets/aqsa.jpg')); ?>">
     <script src="https://cdn.tailwindcss.com"></script>
 
     <script>
