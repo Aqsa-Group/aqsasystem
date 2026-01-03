7c25eaf667f40cc18c2ab918326c474f9859771a
safiullah63 <safiullahkarimi.dev@gmail.com>
2026-01-03 21:24:17 +0430

    panel edited

--- resources/views/Sarafi/Auth/login.blade.php
+++ resources/views/Sarafi/Auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ورود به پنل صرافی</title>
+    <link rel="icon" type="image/jpeg" href="{{ asset('assets/aqsa.jpg') }}">
 
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="{{ asset('assets/js/app.js') }}"></script>
