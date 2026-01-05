785ddbd1da6c9fca8be95de36c792a56b27944bf
safiullah63 <safiullahkarimi.dev@gmail.com>
2026-01-05 17:21:54 +0430

    panel edited

--- resources/views/livewire/sarafi/buy-sell-currency.blade.php
+++ resources/views/livewire/sarafi/buy-sell-currency.blade.php
@@ -48,7 +48,7 @@
                         <tr class="text-black dark:text-white border-b border-[#D9D9D9] bg-transparent">
                             <td class="px-2 py-4 vazir text-[14px] md:text-[16px] font-medium text-center w-16">1</td>
                             @foreach (['usd', 'afn', 'irr', 'eur', 'pkr', 'aed', 'try', 'cny'] as $currency)
-                            <td class="px-2 py-4 vazir text-[14px] md:text-[16px] font-medium text-center " dir="ltr">
+                            <td dir="ltr" class="px-2 py-4 vazir text-[14px] md:text-[16px] font-medium text-center " >
                                 {{ number_format($totalBuy[$currency] ?? 0 ,2) }}
                             </td>
                             @endforeach
@@ -76,7 +76,7 @@
                         <tr class="text-black dark:text-white border-b border-[#D9D9D9] bg-transparent">
                             <td class="px-2 py-4 vazir text-[14px] md:text-[16px] font-medium text-center w-16">1</td>
                             @foreach (['usd', 'afn', 'irr', 'eur', 'pkr', 'aed', 'try', 'cny'] as $currency)
-                            <td class="px-2 py-4 vazir text-[14px] md:text-[16px] font-medium text-center" dir="ltr">
+                            <td dir="ltr" class="px-2 py-4 vazir text-[14px] md:text-[16px] font-medium text-center" dir="ltr">
                                 {{ number_format($totalSell[$currency] ?? 0 ,2) }}
                             </td>
                             @endforeach
