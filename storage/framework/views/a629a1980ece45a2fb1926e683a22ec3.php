6cc2e89591ff5ebb8ea830fa3d928684b55ad64f
safiullah63 <safiullahkarimi.dev@gmail.com>
2026-01-06 14:08:38 +0430

    panel edited

---
+++ storage/framework/views/a629a1980ece45a2fb1926e683a22ec3.php
@@ -0,0 +1,444 @@
+<div>
+    <div class="space-y-4 mb-6">
+        <h1 class="text-[24px] font-medium vazir">صفحه گزارشات معاملات روزانه حسابات و صندوق ها</h1>
+    </div>
+
+
+        <div class="w-full">
+            <div class="bg-[#F5F5F5] dark:bg-black dark:border dark:border-white p-6 rounded-[12px] mx-auto"
+                style="box-shadow: 0px 4px 4px 0px #00000040, 0 0 0 0 #3B82F6;">
+
+    <div class="overflow-x-auto w-full mt-4">
+        <div class="max-h-[600px] overflow-y-auto">
+            <table class="w-full text-sm md:text-base text-left rtl:text-right text-gray-500">
+                <thead class="bg-[#2B65E5] text-white text-[16px] vazir h-16 sticky top-0">
+                    <tr>
+                        <th class="px-4 py-4 font-bold w-16">
+                            <span class="border border-white px-2 py-1 rounded-lg">#</span>
+                        </th>
+                        <th class="px-4 py-4 font-bold">نام حساب</th>
+                        <th class="px-4 py-4 font-bold">نوع معامله</th>
+                        <th class="px-4 py-4 font-bold">نوع حساب</th>
+                        <th class="px-4 py-4 font-bold">مقدار</th>
+                        <th class="px-4 py-4 font-bold">ارز</th>
+                        <th class="px-4 py-4 font-bold">بیلانس</th>
+                        <th class="px-4 py-4 font-bold">توضیحات</th>
+                        <th class="px-4 py-4 font-bold">تاریخ</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr class=" border-b  dark:bg-black dark:text-white dark:border-white hover:bg-gray-50">
+                        <td class="px-4 py-4">
+                            1
+                        </td>
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    مجید محمدی
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                     237432864832432
+                                </div>
+                            </div>
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            رسید
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            نقدی
+                        </td>
+
+                        <td class="px-4 py-4">
+                            40000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            افغانی
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            42000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            مبلغ چهل هزار افغانی از حساب مرتضی به حساب مجید رسید شد
+                        </td>
+
+
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    1404-2-3
+
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                    12:00
+                                </div>
+                            </div>
+                        </td>
+
+
+
+
+
+
+                    </tr>
+
+
+
+
+                       <tr class=" border-b  dark:bg-black dark:text-white dark:border-white hover:bg-gray-50">
+                        <td class="px-4 py-4">
+                            1
+                        </td>
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    مجید محمدی
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                     237432864832432
+                                </div>
+                            </div>
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            رسید
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            نقدی
+                        </td>
+
+                        <td class="px-4 py-4">
+                            40000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            افغانی
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            42000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            مبلغ چهل هزار افغانی از حساب مرتضی به حساب مجید رسید شد
+                        </td>
+
+
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    1404-2-3
+
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                    12:00
+                                </div>
+                            </div>
+                        </td>
+
+
+
+
+
+
+                    </tr>
+
+
+
+                       <tr class=" border-b  dark:bg-black dark:text-white dark:border-white hover:bg-gray-50">
+                        <td class="px-4 py-4">
+                            1
+                        </td>
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    مجید محمدی
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                     237432864832432
+                                </div>
+                            </div>
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            رسید
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            نقدی
+                        </td>
+
+                        <td class="px-4 py-4">
+                            40000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            افغانی
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            42000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            مبلغ چهل هزار افغانی از حساب مرتضی به حساب مجید رسید شد
+                        </td>
+
+
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    1404-2-3
+
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                    12:00
+                                </div>
+                            </div>
+                        </td>
+
+
+
+
+
+
+                    </tr>
+
+
+
+                       <tr class=" border-b  dark:bg-black dark:text-white dark:border-white hover:bg-gray-50">
+                        <td class="px-4 py-4">
+                            1
+                        </td>
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    مجید محمدی
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                     237432864832432
+                                </div>
+                            </div>
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            رسید
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            نقدی
+                        </td>
+
+                        <td class="px-4 py-4">
+                            40000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            افغانی
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            42000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            مبلغ چهل هزار افغانی از حساب مرتضی به حساب مجید رسید شد
+                        </td>
+
+
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    1404-2-3
+
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                    12:00
+                                </div>
+                            </div>
+                        </td>
+
+
+
+
+
+
+                    </tr>
+
+
+
+
+
+                       <tr class=" border-b  dark:bg-black dark:text-white dark:border-white hover:bg-gray-50">
+                        <td class="px-4 py-4">
+                            1
+                        </td>
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    مجید محمدی
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                     237432864832432
+                                </div>
+                            </div>
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            رسید
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            نقدی
+                        </td>
+
+                        <td class="px-4 py-4">
+                            40000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            افغانی
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            42000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            مبلغ چهل هزار افغانی از حساب مرتضی به حساب مجید رسید شد
+                        </td>
+
+
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    1404-2-3
+
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                    12:00
+                                </div>
+                            </div>
+                        </td>
+
+
+
+
+
+
+                    </tr>
+
+
+
+
+
+
+
+                       <tr class=" border-b  dark:bg-black dark:text-white dark:border-white hover:bg-gray-50">
+                        <td class="px-4 py-4">
+                            1
+                        </td>
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    مجید محمدی
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                     237432864832432
+                                </div>
+                            </div>
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            رسید
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            نقدی
+                        </td>
+
+                        <td class="px-4 py-4">
+                            40000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            افغانی
+                        </td>
+
+
+                        <td class="px-4 py-4">
+                            42000
+                        </td>
+
+                        <td class="px-4 py-4">
+                            مبلغ چهل هزار افغانی از حساب مرتضی به حساب مجید رسید شد
+                        </td>
+
+
+                        <td class="px-4 py-4 vazir text-[14px] md:text-[16px] text-center w-40">
+                            <div class="whitespace-nowrap">
+                                <div class="font-medium">
+                                    1404-2-3
+
+                                </div>
+                                <div class="text-gray-500 dark:text-white text-sm mt-1">
+                                    12:00
+                                </div>
+                            </div>
+                        </td>
+
+
+
+
+
+
+                    </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+            </div>
+</div><?php /**PATH /home/safiullah/Documents/GitHub/AqsaSystem/resources/views/livewire/sarafi/journal.blade.php ENDPATH**/ ?>
