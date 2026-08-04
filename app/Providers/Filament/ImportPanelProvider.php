3df5abed5b72145a981f6c7e6381fee70a94ed75
safiullah63 <safiullahkarimi.dev@gmail.com>
2026-08-04 12:22:35 +0430

    panel edited for report

--- app/Providers/Filament/ImportPanelProvider.php
+++ app/Providers/Filament/ImportPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Support\Enums\MaxWidth;
 
 class ImportPanelProvider extends PanelProvider
 {
@@ -39,6 +40,8 @@ class ImportPanelProvider extends PanelProvider
             ])
             ->login(ImportLogin::class)
             ->authGuard('import')
+                ->maxContentWidth(MaxWidth::Full)
+
             ->databaseNotifications()
             ->databaseNotificationsPolling('60s')
             ->brandName("حبیب یونس لمتید")
